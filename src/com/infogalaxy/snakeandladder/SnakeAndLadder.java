1aee9a00e475d9c05d010d5437325e5fd68af338
TanujPatil <tanujpatil9561@gmail.com>
2024-03-07 10:51:40 +0530

    [ADDED]:by Tanuj - Started with single player at position 0

---
+++ src/com/infogalaxy/snakeandladder/SnakeAndLadder.java
@@ -0,0 +1,10 @@
+package com.infogalaxy.snakeandladder;
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+			int pos = 0;
+			System.out.println("The position of a player is: " +pos);
+	}
+
+}
