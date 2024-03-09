cd0902dc9bfd1793058582045e5981457dada8f9
TanujPatil <tanujpatil9561@gmail.com>
2024-03-09 14:38:21 +0530

    [ADDED]:by Tanuj - Checking for options using Random class

--- src/com/infogalaxy/snakeandladder/SnakeAndLadder.java
+++ src/com/infogalaxy/snakeandladder/SnakeAndLadder.java
@@ -1,15 +1,46 @@
 package com.infogalaxy.snakeandladder;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class SnakeAndLadder {
 
-	public static void main(String[] args) {
-		Random random = new Random();
-		int pos = 0;
-		int rollDie = random.nextInt(6) + 1;
-		System.out.println("The position of a player is: " +pos);
-		System.out.println("The position of a player is: " +rollDie);
+	Scanner scanner = new Scanner(System.in);
+	Random random = new Random();
+	int playerPosition = 0;
+	int option;
+	int dieNo;
+	private static final int STAY = 0;
+	private static final int LADDER = 1;
+	private static final int SNAKE = 2;
+	
+	public void show() {
+		System.out.println("Position of a player is: " +playerPosition);
+		dieNo = random.nextInt(6) + 1;
+		option = random.nextInt(3);
+		
+		switch(option) {
+			case STAY:
+				playerPosition = playerPosition;
+				break;
+			case LADDER:
+				playerPosition = playerPosition + dieNo;
+				break;
+			case SNAKE:
+				playerPosition = playerPosition - dieNo;
+				if(playerPosition < 0) {
+					playerPosition = 0;
+				}
+				break;
+		}	
+		System.out.println("The value of a option is = " +option);
+		System.out.println("The number on Die is = " +dieNo);
+		System.out.println("The new position of a player = " +playerPosition);
 	}
-
+	
+	
+	public static void main(String[] args) {
+		SnakeAndLadder player1 = new SnakeAndLadder();
+		player1.show();
+	}
 }
