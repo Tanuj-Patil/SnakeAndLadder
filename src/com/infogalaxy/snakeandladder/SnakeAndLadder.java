cc529f624d677a6d981a1e904f72f6a38db30973
TanujPatil <tanujpatil9561@gmail.com>
2024-03-09 19:00:07 +0530

    [ADDED]:by Tanuj - playing until user wins

--- src/com/infogalaxy/snakeandladder/SnakeAndLadder.java
+++ src/com/infogalaxy/snakeandladder/SnakeAndLadder.java
@@ -15,30 +15,33 @@ public class SnakeAndLadder {
 	private static final int SNAKE = 2;
 	
 	public void show() {
-		System.out.println("Position of a player is: " +playerPosition);
-		dieNo = random.nextInt(6) + 1;
-		option = random.nextInt(3);
-		
-		switch(option) {
-			case STAY:
-				playerPosition = playerPosition;
-				break;
-			case LADDER:
-				playerPosition = playerPosition + dieNo;
-				break;
-			case SNAKE:
-				playerPosition = playerPosition - dieNo;
-				if(playerPosition < 0) {
-					playerPosition = 0;
-				}
-				break;
-		}	
+		while (playerPosition != 100) {
+			System.out.println("Position of a player is: " +playerPosition);
+			dieNo = random.nextInt(6) + 1;
+			option = random.nextInt(3);
+			
+			switch(option) {
+				case STAY:
+					playerPosition = playerPosition;
+					break;
+				case LADDER:
+					if(playerPosition + dieNo > 100) {
+						continue;
+					}
+					playerPosition = playerPosition + dieNo;
+					break;
+				case SNAKE:
+					playerPosition = playerPosition - dieNo;
+					if(playerPosition < 0) {
+						playerPosition = 0;
+					}
+					break;
+			}	
+		}
 		System.out.println("The value of a option is = " +option);
 		System.out.println("The number on Die is = " +dieNo);
 		System.out.println("The new position of a player = " +playerPosition);
 	}
-	
-	
 	public static void main(String[] args) {
 		SnakeAndLadder player1 = new SnakeAndLadder();
 		player1.show();
