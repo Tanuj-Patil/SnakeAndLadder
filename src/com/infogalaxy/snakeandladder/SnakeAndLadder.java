d17e595472bfc79f3bf0c5051cf7b77fa9721548
TanujPatil <tanujpatil9561@gmail.com>
2024-03-11 10:06:54 +0530

    [ADDED]:by Tanuj - calculated total die roll required to win the game

--- src/com/infogalaxy/snakeandladder/SnakeAndLadder.java
+++ src/com/infogalaxy/snakeandladder/SnakeAndLadder.java
@@ -10,14 +10,17 @@ public class SnakeAndLadder {
 	int playerPosition = 0;
 	int option;
 	int dieNo;
+	int dieCount = 0;
 	private static final int STAY = 0;
 	private static final int LADDER = 1;
 	private static final int SNAKE = 2;
+	private static final int WINNING_NO = 100;
 	
 	public void show() {
-		while (playerPosition != 100) {
+		while (playerPosition != WINNING_NO) {
 			System.out.println("Position of a player is: " +playerPosition);
 			dieNo = random.nextInt(6) + 1;
+			dieCount++;
 			option = random.nextInt(3);
 			
 			switch(option) {
@@ -25,6 +28,9 @@ public class SnakeAndLadder {
 					playerPosition = playerPosition;
 					break;
 				case LADDER:
+					if(playerPosition + dieNo > WINNING_NO) {
+						continue;
+					}
 					playerPosition = playerPosition + dieNo;
 					break;
 				case SNAKE:
@@ -38,6 +44,7 @@ public class SnakeAndLadder {
 		System.out.println("The value of a option is = " +option);
 		System.out.println("The number on Die is = " +dieNo);
 		System.out.println("The new position of a player = " +playerPosition);
+		System.out.println("The total die required to win the game is = " +dieCount);
 	}
 	public static void main(String[] args) {
 		SnakeAndLadder player1 = new SnakeAndLadder();
